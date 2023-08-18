843b8f6571427b9f9dc20564a7d72fa0d0e56538
Viorel Rînja <viorel.rinja@gmail.com>
2023-08-18 13:22:52 +0300

    update packages

--- components/molecules/search-form/search-form.spec.tsx
+++ components/molecules/search-form/search-form.spec.tsx
@@ -1,9 +1,10 @@
 import { render } from '@testing-library/react';
+import { noop } from '@spc/utils/noop';
 import { SearchForm } from './search-form.component';
 
 describe('SearchForm', () => {
   it('Should render with empty state', () => {
-    const { baseElement } = render(<SearchForm />);
+    const { baseElement } = render(<SearchForm onChange={noop} />);
     expect(baseElement).toBeTruthy();
   });
 });
